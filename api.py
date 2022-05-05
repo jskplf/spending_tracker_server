75c3ecb5e507911cbab64302daf3345315d4e5fc
jared <jskplf@gmail.com>
2022-05-05 19:56:29 -0400

    Intial Commit

---
+++ api.py
@@ -0,0 +1,104 @@
+import json
+import os
+import pickle
+import traceback
+from typing import List
+from fastapi import FastAPI, File,UploadFile
+
+from pandas import DataFrame, read_pickle
+from tqdm import tqdm
+import processing
+from PIL import Image
+from io import BytesIO
+
+app = FastAPI(title='OCR Api', description='An api for interacting with a receipt processing OCR')
+
+DATASET = 'dataset/'
+SERIALIZED_PATH = 'dataset.pck' # a serialized version of the results from the initial dataset
+
+# As long as the dataset hasn't gained any new files don't reprocess it
+d = []
+try:
+    d = pickle.load(open(SERIALIZED_PATH, 'rb'))
+except:
+    # Initialize the receipt image data set
+    dataset = os.listdir(DATASET)
+
+    d = []
+    # Process each image in the dataset
+    for image in tqdm(dataset, f'Loading initial dataset from {DATASET}'):
+        image = f'{DATASET}{image}'
+        try:
+            d.append(processing.process_image(Image.open(image)))
+        except:
+            traceback.print_exc()
+            print(f'{image} gave an error')
+    
+    # Serialize the dataset so that it doesn't need to re processed the next time the server starts
+    pickle.dump(d,open(SERIALIZED_PATH,'wb'))
+        
+# Put data set results into a dataframe 
+df = DataFrame(d)
+
+app = FastAPI(title='Spending Tracker OCR API', description='An api to process a receipt through ocr',version=1.0)
+
+@app.get('/')
+async def index():
+    """
+        Root endpoint of the api
+        - can be used to test if the endpoint is working
+    
+        1. Use threads to a check for one regex on whole document, multiple workers = multiple regex and matchs being found at the same time
+        - each type a regex tests positive for a field give it a vote
+        - assume break up the line based on the amount of votes a type gets 
+        - issue is that 
+    """
+    return {'status_code': 200, 'message': 'Welcome to 370 backend'} 
+
+@app.post('/ocr/')
+async def upload(files: List[UploadFile] = File(..., description='Multiple images uploaded for processing ')):
+    images = [await f.read() for f in files]
+    data = []
+    for image in images:
+        data.append(processing.process_image(Image.open(BytesIO(image))))
+
+    return {"status_code" : 200, "data": data}
+
+@app.get('/receipt_data/')
+async def receipt_data_view():
+    """
+        return all data collected from proccessing the free 
+        receipt database with tesseract ocr
+        - [Link to dataset](https://expressexpense.com/blog/free-receipt-images-ocr-machine-learning-dataset/)
+    """
+
+    df_stats = {}
+    df_stats['success_rate'] = get_success()
+
+    data = {'stats': df_stats,'contents': json.loads(df.to_json())}
+
+    return{'status_code': 200, 'data': data}
+
+@app.get('/sample/')
+def get_sample(size: int = 5):
+    """
+        Returns @size number of random receipts that are in the dataset
+    """
+
+    return {'status_code': 200, 'data': json.loads(df.head(size).to_json())}
+
+
+# Put this in processing and give each receipt a likelihood grade
+def get_success():
+    """
+        Calculate and the return a list of all the success rates fro each parameter
+        - Success rate = Number of times column is not missing a value / Total number of rows
+    """
+    out = {}
+    for col in df.columns:
+        rate = df[col].notnull().sum()/len(df) 
+        if rate == 1 or rate == 0:
+            out[col] = None
+        else:
+            out[col] = rate
+    return out
---
+++ processing.py
@@ -0,0 +1,109 @@
+import os
+import pickle
+import re
+from PIL import Image
+import pytesseract
+
+def load_data(image: Image):
+    '''
+        Process an image for data using pytesseract
+        returns lots of information with the most control 
+        for custom piecing together the image
+    '''
+
+   
+    data = pytesseract.image_to_data(image)
+    return data
+
+
+def process_image(image: Image):
+    """
+        Run custom regex on each line of the receipt 
+        and attempt to generate a transaction entry
+
+        ```mermaid
+        sequenceDiagram
+        teseract->lines
+        line->parser
+        parser->reciept
+        ```
+    """
+    text = pytesseract.image_to_string(image)
+    lines = text.split('\n\n')
+    data = {}
+    data['store'] = lines[0]
+    max_newlines = None
+    for l in lines:
+        if 'SUBTOTAL' in l.upper() or 'TOTAL':
+            data['total'] = l.split()
+        if 'TEND' in l:
+            data['purchase_method'] = l.split()
+
+        # Find a possible dates in the receipt
+        # Reg expression only works for 00/00/00 formatted dates
+        if re.search('\d\d/\d\d/\d\d', l) != None:
+            for part in l.split():
+                if re.search('\d\d/\d\d/\d\d', part) != None:
+                    data['parsed_date'] = part
+        if max_newlines == None:
+            max_newlines = l
+        else:
+            # Is it possible for a line with no items to have the same new lines as a line with items?
+            if max_newlines.count('\n') == l.count('\n'):
+                pass
+            if max_newlines.count('\n') < l.count('\n'):
+                max_newlines = l
+
+    # Assume that the line with the most new lines has the items sold
+    data['items'] = max_newlines.split('\n')
+
+    # Parse the item name product code and amount
+    for index,i in enumerate(data['items']):
+        item = {}
+        item['name'] = ''
+        item['product_code'] = ''
+        item['amount'] = ''
+        
+        # Assume all letters in an item are part of an items name
+        for c in i:
+            # Take only letters, no spaces
+            if c.isdigit() is not True:
+                item['name'] += c
+        
+        # Find the product code of the item, it should be a long string of only numbers
+        split_item = i.split()  
+
+        # Find the amount the item cost
+        for x in split_item :
+            
+            if x.isdecimal():
+                item['product_code'] = x
+            
+            if '.' in x:
+                item['amount'] = x
+
+        data['items'][index] = item
+    return data
+
+def files(path : str):
+    for f in os.lisdir(path):
+        yield f
+
+
+def dataset(path: str = 'dataset/'):
+    '''
+        Process all the files in the directory
+    '''
+    [load_data([files(path)])]
+
+def to_pck(dataset = dataset):
+    '''
+        Serialize the collected data to a binary form
+    '''
+    pickle.dump([dataset()],open('dataset.pck','wb'))
+
+def from_pck(path: str = 'dataset.pck'):
+    '''
+        Load the data back from the pickle file
+    '''
+    return pickle.load(open(path))
